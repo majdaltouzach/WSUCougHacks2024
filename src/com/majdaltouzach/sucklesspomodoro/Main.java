c3917a5905df5c7b656adf4c8f558b79398f8d64
majdaltouzach <majdaltoufic88@tutanota.com>
2024-04-05 19:23:20 -0700

    Recreated JavaFx project

---
+++ src/com/majdaltouzach/sucklesspomodoro/Main.java
@@ -0,0 +1,13 @@
+package com.majdaltouzach.sucklesspomodoro;
+
+
+//PATH C://Users//Rafi//eclipse-workspace//Productivity++//src//Main//
+
+public class Main {
+	public static void main(String[] a) {
+    	
+        LoginFrame lframe = new LoginFrame();
+        lframe.setVisible(true);
+
+    }
+}
